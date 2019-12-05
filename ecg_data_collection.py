6a59274e9cea11f8fb8e831f07f02ed9080d69b6
rai-anshul <anshulrai2001@gmail.com>
2019-12-05 10:50:30 +0000

    minor changes

--- ecg_data_collection.py
+++ ecg_data_collection.py
@@ -11,10 +11,15 @@ f = signal.resample(x=y, num=1000, t=x)
 sig = f[0]*5
 Fs = mdata['sampling_rate']
 out = ecg.ecg(signal=sig, sampling_rate=Fs, show=True)
+
+r_peak = out[2]
+
+print(out[2])
 N = 1000
 
 f_dash, Pxx_den = signal.welch(sig)
-pl.semilogy(f_dash, Pxx_den)
+# print(f_dash, Pxx_den)
+# pl.semilogy(f_dash, Pxx_den)
 # pl.ylim([0.5e-3, 1])
 pl.xlabel('frequency [Hz]')
 pl.ylabel('PSD [V**2/Hz]')
