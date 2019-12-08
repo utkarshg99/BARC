63822e2a81fa408e87ebc45cb9c715372d94b783
rai-anshul <anshulrai2001@gmail.com>
2019-12-08 23:35:31 +0000

    added serial read code

---
+++ data_python.py
@@ -0,0 +1,17 @@
+import serial
+import csv
+
+ser = serial.Serial()
+ser.baudrate=115200
+ser.port="/dev/ttyUSB0"
+ser.open()
+# for i in range (100):
+#     val =  ser.readline().decode("utf-8")
+# print(val)
+with open("output.csv", "a") as fp:
+    wr = csv.writer(fp, dialect='excel')
+    while(True):
+        val =  ser.readline().decode("utf-8").split(',')
+        val[2] = val[2].rstrip('\r\n')
+        print((val))
+        wr.writerow(val)
--- ecg_data_collection.py
+++ ecg_data_collection.py
@@ -3,29 +3,28 @@ import numpy as np
 from biosppy.signals import ecg
 import matplotlib.pyplot as pl
 from biosppy import storage
+import pandas
 
 y, mdata = storage.load_txt('./values.txt')
 x, mdata = storage.load_txt('./tme.txt')
-f = signal.resample(x=y, num=1000, t=x)
+f = signal.resample(x=y, num=80000, t=x)
 
+var = pandas.read_csv('anshul_02.csv')
+x = np.array(var['time(milli)'])
+y = np.array(var['Volt'])
 sig = f[0]*5
 Fs = mdata['sampling_rate']
 out = ecg.ecg(signal=sig, sampling_rate=Fs, show=True)
 
-r_peak = out[2]
+r_peak = out[2] #stores
 
-# print(out[2])
-
+print(out[2])
 
 f_dash, Pxx_den = signal.welch(sig)
-temp = signal.welch(sig)
-# print(f_dash, Pxx_den)
-print(f_dash)
-print(np.shape(temp))
+print(f_dash.shape, Pxx_den.shape)
 pl.semilogy(f_dash, Pxx_den)
 # pl.ylim([0.5e-3, 1])
+# pl.plot(f_dash, Pxx_den)
 pl.xlabel('frequency [Hz]')
 pl.ylabel('PSD [V**2/Hz]')
 pl.show()
-
-# print(f_dash)
